cf02722c659ff7cf94d787ece18f68136f5301ea
moneem79 <moneemmonower79@gmail.com>
2025-06-09 00:41:21 +0600

    1st programe about kotlin flow

---
+++ app/src/main/java/com/example/kotlinflow/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.kotlinflow
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.kotlinflow.ui.theme.KotlinFlowTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinFlowTheme {
+                val viewModel = viewModels<MyviewModel>()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+                        Column {
+                            val textfeildvalue = remember { mutableStateOf(-5) }
+
+                            Text(text = textfeildvalue.value.toString())
+
+
+
+
+                            lifecycleScope.launch() {
+                                    delay(5000)
+
+                            viewModel.value.sharedFlow.collect{
+                                    textfeildvalue.value = it
+
+                            }
+
+
+
+
+
+                        }
+                    }
+                }
+            }}}}}
---
+++ app/src/main/java/com/example/kotlinflow/viewModel.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinflow
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+
+class MyviewModel: ViewModel() {
+
+    val sharedFlow = MutableSharedFlow<Int>()
+    init {
+        emitvalue()
+    }
+
+
+    fun emitvalue() {
+
+        viewModelScope.launch {
+        repeat(10){
+            delay(1000)
+            sharedFlow.emit(it)
+
+        }
+    }
+}
+}
